b967ff615d63821fad7d89bec2fe48f27626581b
D'Amato, Edward <info@edwarddamato.com>
2017-03-09 21:06:22 +0000

    Play around with Types

--- app/App.elm
+++ app/App.elm
@@ -12,5 +12,44 @@ main =
   div []
     [ Html.node "link" [ Html.Attributes.rel "stylesheet", Html.Attributes.href "app.css" ] [] 
     ,h1 [ class [ SharedStyles.Title ] ] [ text "Hello, there!" ]
-    , div [] [ text "moo2" ]
+    , div [] [ text "Action" ]
+    , div [] [ text myOp ]
     ]
+
+
+type OpType = 
+    Test
+    | Remove
+    | Add
+    | Replace
+    | Move
+    | Copy
+
+type OpVal =
+    String
+    | Int
+    | Bool
+
+type alias OpAttr = { from : Maybe String, path : Maybe String, value : Maybe OpVal }
+
+type alias Op = { action : OpType, attr : OpAttr }
+
+doOp : Op -> String
+doOp myOp =
+    case myOp.action of
+        Test ->
+            "tested!"
+        Remove ->
+            "removed! with: " ++ Maybe.withDefault "" myOp.attr.path
+        Add ->
+            "added!"
+        Replace ->
+            "replaced!"
+        Move ->
+            "moved!"
+        Copy ->
+            "copied!"
+
+myOp : String
+myOp =
+    doOp { action = Remove, attr = { from = Nothing, path = Just "moo/cow", value = Nothing } }
